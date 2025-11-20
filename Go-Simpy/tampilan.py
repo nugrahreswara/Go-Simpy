49197a5baa20ca11bda73811a3d30b93834d990b
miawicey <vitakhairana@gmail.com>
2025-11-20 21:42:40 +0800

    menyelesaikan

--- Go-Simpy/tampilan.py
+++ Go-Simpy/tampilan.py
@@ -16,15 +16,8 @@ def banner(text):
 
 
 def tampilkan_menu_login():
-	bersihkan_layar()
-	banner("MENU LOGIN")
-	print(f"{BULLET} 1. Login")
-	print(f"{BULLET} 2. Registrasi Customer")
-	print(f"{BULLET} 3. Registrasi Driver")
-	print(f"{BULLET} 4. Keluar")
-	print(LINE)
-
-<<<<<<< HEAD
+    bersihkan_layar()
+    banner("MENU LOGIN")
     print(f"{BULLET} 1. Login")
     print(f"{BULLET} 2. Registrasi Customer")
     print(f"{BULLET} 3. Registrasi Driver")
@@ -68,41 +61,3 @@ def tampilkan_menu_utama(role):
         print(f"{BULLET} 5. Logout")
         print(f"{BULLET} 6. Keluar")
         print(LINE)
-=======
-def tampilkan_menu_utama():
-	bersihkan_layar()
-	banner(f"Selamat Datang")
-	role = dapatkan_data_akun(apa_akun_sekarang())["role"]
-    
-	if role == "admin":
-		print(" MENU ADMIN")
-		print(LINE)
-		print(f"{BULLET} 1. Edit Profil")
-		print(f"{BULLET} 2. Tambah Akun Customer")
-		print(f"{BULLET} 3. Tambah Akun Driver")
-		print(f"{BULLET} 4. Hapus User")
-		print(f"{BULLET} 5. Lihat Semua Akun")
-		print(f"{BULLET} 6. Logout")
-		print(f"{BULLET} 7. Keluar")
-		print(LINE)
-    
-	elif role == "customer":
-		print(" 	MENU CUSTOMER")
-		print(LINE)
-		print(f"{BULLET} 1. Edit Profil")
-		print(f"{BULLET} 2. Cek Saldo")
-		print(f"{BULLET} 3. Pesan Ojek")
-		print(f"{BULLET} 4. Logout")
-		print(f"{BULLET} 5. Keluar")
-		print(LINE)
-    
-	elif role == "driver":
-		print("   MENU DRIVER")
-		print(LINE)
-		print(f"{BULLET} 1. Edit Profil")
-		print(f"{BULLET} 2. Cek Saldo")
-		print(f"{BULLET} 3. Lihat Pendapatan")
-		print(f"{BULLET} 4. Logout")
-		print(f"{BULLET} 5. Keluar")
-		print(LINE)
->>>>>>> 1d974406ae377da7da34b5023b9646b6a3f6308c
